d03966cee98284fadb3b6c1bcab6cbc5ed75e75c
Pedro Henrique <pedrohenriquesampaiodenovaes@gmail.com>
2022-10-18 12:53:40 -0300

    Corrigido bug que não permitia pegar os números do campo de número e aparecer no cartão

--- src/js/index.js
+++ src/js/index.js
@@ -2,7 +2,7 @@ import validationForm from './validation-form.js';
 import sendForm from './send-form.js';
 
 const nome = validationForm('#nome', null, '.wrapper-dados p');
-const numero = validationForm('#numero', 'numero', '.digitos', 16);
+const numero = validationForm('#numero', 'numero', '.digits', 16);
 const month = validationForm('#mes', 'mes', '.month');
 const year = validationForm('#ano', 'ano', '.year');
 const cvc = validationForm('#cvc', 'cvc', '.cvc', 3);
